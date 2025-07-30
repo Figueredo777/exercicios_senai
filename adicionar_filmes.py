07f23e5adb864de6ed4a895c24ac7b882d13c847
Figueredo777 <gsfigueredo2008@gmail.com>
2025-07-30 13:15:51 -0300

    Add files via upload

---
+++ adicionar_filmes.py
@@ -0,0 +1,6 @@
+adicionar_filmes = int(input("Digite a quantidade de filmes: " ))
+contador = 1
+filme = input("Digite o nome do filme: ")
+while contador <= adicionar_filmes:
+    
+    contador = contador + 1
---
+++ verificador_de_senha_1.py
@@ -0,0 +1,8 @@
+senha = input("Digite a senha correta: ")
+if senha != "san220108":
+    while senha != "san220108":
+        print("Senha incorreta")
+        senha = input("Digite a senha correta: ")
+
+
+print("Senha correta")
